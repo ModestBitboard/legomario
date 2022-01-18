f6730d309796a4ae8f62cebb190ed5c8255882e0
ShadowFire5650 <94723549+ShadowFire5650@users.noreply.github.com>
2022-01-18 09:56:00 -0800

    Added compatibility to new Lego Mario barcodes
    I need to come by and change the phrase "New" to a variable hex code.

--- legomario/mario.py
+++ legomario/mario.py
@@ -11,13 +11,19 @@ SUBSCRIBE_RGB_COMMAND = bytearray([0x0A, 0x00, 0x41, 0x01, 0x00, 0x05, 0x00, 0x0
 class Mario:
 
     def __init__(self):
+        self._newTileEventHooks = []
         self._tileEventHooks = []
         self._accelerometerEventHooks = []
         self._doLog = False
         self._run = True
+        self._doHex = False
 
     def _signed(self, char):
         return char - 256 if char > 127 else char
+
+    def _hex(self, msg):
+        if self._doHex:
+            print(str(msg))
 
     def _log(self, msg):
         if self._doLog:
@@ -26,6 +32,9 @@ class Mario:
     def AddTileHook(self, func):
         self._tileEventHooks.append(func)
 
+    def AddNewTileHook(self, func):
+        self._newTileEventHooks.append(func)
+
     def AddAccelerometerHook(self, func):
         self._accelerometerEventHooks.append(func)
 
@@ -33,18 +42,25 @@ class Mario:
         for func in self._tileEventHooks:
             func(v)
 
+    def _callNewTileHooks(self, v):
+        for func in self._newTileEventHooks:
+            func(v)
+
     def _callAccelerometerHooks(self, x, y, z):
         for func in self._accelerometerEventHooks:
             func(x, y, z)
 
     def _handle_events(self, sender, data):
+        self._hex("Hex: " + " ".join(hex(n) for n in data))
         # Camera sensor data
         if data[0] == 8:
             # RGB code
             if data[5] == 0x0:
-                self._log(f"Barcode: {hex(data[4])}")
+                self._log("Barcode: " + " ".join(hex(n) for n in data))
                 self._callTileHooks(hex(data[4]))
-
+            elif data[5] == 0x1:
+                self._log("Barcode (New): " + " ".join(hex(n) for n in data))
+                self._callNewTileHooks(hex(data[4]))
         # Accelerometer data
         elif data[0] == 7:
             x = int(self._signed(data[4]))
